4a609f2c7377c911d0e70b0d05dc3ddee2400021
Kontiki007 <86094519+Kontiki007@users.noreply.github.com>
2021-06-24 23:19:37 +0930

    3rd modification

---
+++ Local Notebook/Note pages/example.php
@@ -0,0 +1,11 @@
+<?php require("../header.php")?>
+
+<div class="col-8 black">
+	<h3 class="blue">
+		9.2 Phloem is what?!
+	</h3>
+	<mark>Hey, what can you expect from an example page?</mark>
+	<span class="black">
+		Try this out!
+	</span>
+</div>
